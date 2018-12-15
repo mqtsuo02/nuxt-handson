6d75288f1470c3682be80417b36634fc9bc1713c
mqtsuo02 <obu.tokyo@gmail.com>
2018-12-16 01:16:04 +0900

    add Form

--- 04-vuetify/pages/items/new.vue
+++ 04-vuetify/pages/items/new.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>Items New</h1>
+  <div>
+    <h1>Items New</h1>
+    <v-form>
+      <v-text-field
+        v-model="name"
+        label="name"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="price"
+        label="price"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="description"
+        label="description"
+        required
+      ></v-text-field>
+    </v-form>
+    <v-btn color="success" @click="add({name, price, description})">Add</v-btn>
+  </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      name: "",
+      price: null,
+      description: ""
+    };
+  },
+  methods: {
+    add(item) {
+      console.log(item)
+    }
+  }
+};
+</script>
